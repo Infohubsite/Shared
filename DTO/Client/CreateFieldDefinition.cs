b6fe8739893b5a6f8e51b3dbc90925e33bfe8574
Infohubsite <216588469+Infohubsite@users.noreply.github.com>
2025-09-15 21:24:36 +0300

    Now using the right DataType

--- DTO/Client/CreateFieldDefinition.cs
+++ DTO/Client/CreateFieldDefinition.cs
@@ -6,7 +6,7 @@ namespace Shared.DTO.Client
     public record CreateFieldDefinition
     {
         [JsonPropertyName("name")][Required(ErrorMessage = "Field name is required")][StringLength(100, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 100 characters")] public required string Name { get; init; }
-        [JsonPropertyName("dataType")][Required(ErrorMessage = "Data type is required")] public required DataType DataType { get; init; }
+        [JsonPropertyName("dataType")][Required(ErrorMessage = "Data type is required")] public required Enum.DataType DataType { get; init; }
         [JsonPropertyName("isRequired")] public bool IsRequired { get; init; } = true;
         [JsonPropertyName("isList")] public bool IsList { get; init; } = false;
         [JsonPropertyName("referenceTargetEntityDefinitionId")] public Guid? ReferenceTargetEntityDefinitionId { get; init; }
