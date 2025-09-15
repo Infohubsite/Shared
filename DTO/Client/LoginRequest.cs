35fe91e0cb20f656260d46a700edfa80352ff3a2
Infohubsite <216588469+Infohubsite@users.noreply.github.com>
2025-09-15 20:59:50 +0300

    Added login request for client

---
+++ DTO/Client/LoginRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Shared.DTO.Client
+{
+    public record LoginRequest
+    {
+        [JsonPropertyName("username")][Required(ErrorMessage = "Username is required")] public required string Username { get; init; }
+        [JsonPropertyName("password")][Required(ErrorMessage = "Password is required")] public required string Password { get; init; }
+    }
+}
